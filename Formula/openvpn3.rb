c666db20a532cbc6c54a2e68ab9a8708494718e6
Tomislav Mihaliček <tomislav.mihalicek@intis.hr>
2026-01-28 10:29:17 +0100

    Make default install work without --HEAD flag

--- Formula/openvpn3.rb
+++ Formula/openvpn3.rb
@@ -1,6 +1,11 @@
 class Openvpn3 < Formula
   desc "C++ OpenVPN client and library"
   homepage "https://github.com/OpenVPN/openvpn3"
+  url "https://github.com/OpenVPN/openvpn3/archive/refs/heads/master.tar.gz"
+  version "3.10.1"  # Use a version that makes sense
+  sha256 :no_check  # GitHub generates different hashes for branch archives
+  license "AGPL-3.0-only"
+  
   head "https://github.com/OpenVPN/openvpn3.git", branch: "master"
 
   depends_on "asio"
@@ -14,12 +19,13 @@ class Openvpn3 < Formula
 
   def install
     openssl_root = Formula["openssl@3"].opt_prefix
-    cmake_prefix = HOMEBREW_PREFIX
+    
+    args = %W[
+      -DOPENSSL_ROOT_DIR=#{openssl_root}
+      -DCMAKE_PREFIX_PATH=#{HOMEBREW_PREFIX}
+    ]
 
-    system "cmake", "-S", ".", "-B", "build",
-                    "-DOPENSSL_ROOT_DIR=#{openssl_root}",
-                    "-DCMAKE_PREFIX_PATH=#{cmake_prefix}",
-                    *std_cmake_args
+    system "cmake", "-S", ".", "-B", "build", *args, *std_cmake_args
     system "cmake", "--build", "build"
     
     bin.install "build/test/ovpncli/ovpncli"
